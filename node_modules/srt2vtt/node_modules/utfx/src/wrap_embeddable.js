68e6a6be0b9bf9ab5ed4d73bb8491b55253d76dd
Brian <ardilesbrian@gmail.com>
2016-08-21 14:49:17 -0300

    add modules

---
+++ node_modules/srt2vtt/node_modules/utfx/src/wrap_embeddable.js
@@ -0,0 +1,13 @@
+//? UTFX_STANDALONE = false;
+/**
+ * utfx-embeddable (c) 2014 Daniel Wirtz <dcode@dcode.io>
+ * Released under the Apache License, Version 2.0
+ * see: https://github.com/dcodeIO/utfx for details
+ */
+var utfx = function() {
+    "use strict";
+
+    //? include("utfx.js");
+
+    return utfx;
+}();
