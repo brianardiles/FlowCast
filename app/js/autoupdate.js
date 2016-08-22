6691202b3d7e8db7c2d5123b993f5b60b62fdc9e
Brian <ardilesbrian@gmail.com>
2016-08-22 03:40:11 -0300

    fix recent update, beauty

--- app/js/autoupdate.js
+++ app/js/autoupdate.js
@@ -1,80 +1,82 @@
 var remoteVersion,
-    localVersion,
-    autoupdate,
-    localDev = false;
+  localVersion,
+  autoupdate,
+  localDev = false;
 
 debug(localDev);
 
-function debug(localDev){
-  if(localDev == true){
-    throw new ('[INFO]: DEBUG MODE');
+function debug(localDev) {
+  if (localDev == true) {
+    throw new('[INFO]: DEBUG MODE');
   }
 }
 
 var gitrepo = "FlowCast";
 
 var request = require("request"),
-    getpkg = require('../package.json');
+  getpkg = require('../package.json');
 
 localVersion = getpkg.version;
-var url = "https://raw.githubusercontent.com/brai4u/"+gitrepo+"/master/package.json"
+var url = "https://raw.githubusercontent.com/brai4u/" + gitrepo + "/master/package.json"
 
 request({
-    url: url,
-    json: true
-}, function (error, response, json) {
+  url: url,
+  json: true
+}, function(error, response, json) {
 
-    if (!error && response.statusCode === 200) {
-      if(json.developerMode == true){
-        console.log("developer mode");
-        return 1;
-      }
-      autoupdate = json.autoupdate;
-      if(json.version != localVersion){
-        remoteVersion = json.version;
-        updateFlow();
-      }
-      else{
-        console.log("Updated!")
-      }
+  if (!error && response.statusCode === 200) {
+    if (json.developerMode == true) {
+      console.log("developer mode");
+      return 1;
     }
+    autoupdate = json.autoupdate;
+    if (json.version != localVersion) {
+      remoteVersion = json.version;
+      updateFlow();
+    } else {
+      console.log("Updated!")
+    }
+  }
 })
 
-function updateFlow(){
-var fileSize,
+function updateFlow() {
+  var fileSize,
     size = 0;
-console.log('https://github.com/brai4u/'+gitrepo+'/archive/v'+autoupdate+'.zip')
-request('https://github.com/brai4u/'+gitrepo+'/archive/v'+autoupdate+'.zip')
-  .on('response', function(response) {
+  console.log('https://github.com/brai4u/' + gitrepo + '/archive/v' + autoupdate + '.zip')
+  request('https://github.com/brai4u/' + gitrepo + '/archive/v' + autoupdate + '.zip')
+    .on('response', function(response) {
       fileSize = response.headers['content-length'];
     })
 
-  .on('data', function (data){
-    size +=data.length;
+  .on('data', function(data) {
+    size += data.length;
     var porcentaje = size * 100 / fileSize;
 
-    if(isNaN(porcentaje)){
+    if (isNaN(porcentaje)) {
       $("#noti").hide();
+      throw new('[INFO]: No update avalible for someone reason');
+    } else {
+      $("#noti").show();
     }
 
-    $("#noti").show();
     $("#noti").html("Updating: " + porcentaje.toFixed(0) + "%")
     console.log(porcentaje.toFixed(0))
   })
 
   .pipe(fs.createWriteStream('autoupdate.zip'))
-  .on('close', function () {
-    console.log('Dowloaded: v' + autoupdate);
-    installUpdate();
-  });
+    .on('close', function() {
+      console.log('Dowloaded: v' + autoupdate);
+      installUpdate();
+    });
 }
-function installUpdate(){
-    $("#noti").html("Installing update");
-    var AdmZip = require('adm-zip');
-    var zip = new AdmZip("./autoupdate.zip");
-    console.log(gitrepo + "-" +autoupdate)
-    zip.extractEntryTo(gitrepo + "-" +autoupdate+"/app/", "./app", false, true);
-    zip.extractEntryTo(gitrepo + "-" +autoupdate+"/node_modules/", "./node_modules", false, true);
-    zip.extractEntryTo(gitrepo + "-" +autoupdate+"/package.json", "./", false, true);
-    $("#noti").html("Please restart FlowCast!");
+
+function installUpdate() {
+  $("#noti").html("Installing update");
+  var AdmZip = require('adm-zip');
+  var zip = new AdmZip("./autoupdate.zip");
+  console.log(gitrepo + "-" + autoupdate)
+  zip.extractEntryTo(gitrepo + "-" + autoupdate + "/app/", "./app", false, true);
+  zip.extractEntryTo(gitrepo + "-" + autoupdate + "/node_modules/", "./node_modules", false, true);
+  zip.extractEntryTo(gitrepo + "-" + autoupdate + "/package.json", "./", false, true);
+  $("#noti").html("Please restart FlowCast!");
 }
