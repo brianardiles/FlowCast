d8bc3c41378b431b85146377bd14064d10ccaab8
Brian <ardiles.brian@gmail.com>
2016-08-24 03:12:46 -0300

    bump path v1.8-4

--- app/js/autoupdate.js
+++ app/js/autoupdate.js
@@ -17,27 +17,32 @@ var request = require("request"),
   getpkg = require('../package.json');
 
 localVersion = getpkg.version;
-var url = "https://raw.githubusercontent.com/brai4u/" + gitrepo + "/master/package.json"
+var url = "https://raw.githubusercontent.com/brai4u/" + gitrepo + "/master/package.json";
 
-request({
-  url: url,
-  json: true
-}, function(error, response, json) {
+var checkUpdate = setInterval(function(){ requestUpdate() }, 5000);
 
-  if (!error && response.statusCode === 200) {
-    if (json.developerMode == true) {
-      console.log("developer mode");
-      return 1;
+function requestUpdate(){
+  request({
+    url: url,
+    json: true
+  }, function(error, response, json) {
+
+    if (!error && response.statusCode === 200) {
+      if (json.developerMode == true) {
+        console.log("developer mode");
+        return 1;
+      }
+      autoupdate = json.autoupdate;
+      if (json.version != localVersion) {
+        remoteVersion = json.version;
+        updateFlow();
+      } else {
+        console.log("No need a update");
+        clearInterval(checkUpdate);
+      }
     }
-    autoupdate = json.autoupdate;
-    if (json.version != localVersion) {
-      remoteVersion = json.version;
-      updateFlow();
-    } else {
-      console.log("No need a update")
-    }
-  }
-})
+  })
+}
 
 function updateFlow() {
   var fileSize,
@@ -46,6 +51,7 @@ function updateFlow() {
   request('https://github.com/brai4u/' + gitrepo + '/archive/v' + autoupdate + '.zip')
     .on('response', function(response) {
       fileSize = response.headers['content-length'];
+      clearInterval(checkUpdate);
     })
 
   .on('data', function(data) {
