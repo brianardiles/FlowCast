43688acb8ae8ccfa3a286bf5f90cac38dfa9ee4e
Brian Ardiles <ardiles.brian@gmail.com>
2018-10-10 22:17:57 -0300

    add support for notifications

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 // Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron');
 
+app.setAppUserModelId('com.flowcast');
+
 // call autoupdater
 const {autoUpdater} = require('electron-updater');
 autoUpdater.checkForUpdatesAndNotify();
